6a44c643192c9fa42f7b1142d21b006cc361cdf7
SanketTambe39 <sankettambe39@gmail.com>
2021-08-26 11:17:27 +0530

    ADDED: Initial files

---
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class FirstController {
+
+    @RequestMapping("/world")
+    public String sayHello(){
+        return "Hey World Hello !!";
+    }
+
+}
