2a5b4eb0eb4d5a8b8b7b83a3efb9dadac0bad89c
SanketTambe39 <sankettambe39@gmail.com>
2021-08-26 11:42:33 +0530

    ADDED: REST Call to show name

--- src/main/java/com/example/demo/controller/FirstController.java
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -8,8 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class FirstController {
+
+    http://localhost:8081/hello/world
     @RequestMapping("/world")
     public String sayHello(){
         return "Hey World Hello From Bridgelabz !!";
     }
+    // http://localhost:8081/hello/query?name=sanket
+    @RequestMapping(value = {"/query"},method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name){
+        return "Hey "+name+" Hope you are good";
+    }
+    
 }
